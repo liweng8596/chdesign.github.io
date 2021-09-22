7fad93676d0ce04f282ad32f22218bbb3b0ee3cd
lianne <lianne@gmail.com>
2021-09-22 16:23:13 +0800

    init

---
+++ js/all.js
@@ -0,0 +1,16 @@
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+        
+
+  <script>
+  M.AutoInit();
+</script>
